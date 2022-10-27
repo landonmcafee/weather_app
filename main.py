86f418408e2b3d04687d7f9542041243e49d77b5
Landon <mcafeelandon@gmail.com>
2022-10-27 12:43:34 -0500

    Initial commit config.py remains hidden for key privacy

---
+++ main.py
@@ -0,0 +1,31 @@
+# basic beginnings of a weather app 
+
+import requests
+import config
+
+# api key from openweathermap.org
+api_key = config.api_key
+
+user_input = input('Enter city: ')
+# print(user_input) testing printing to console
+
+# fetch url and store in variable
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+while weather_data.json()['cod'] == '404':
+    print('No city found.')
+    user_input = input('Enter city: ')
+    weather_data = requests.get(
+        f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+    # print(weather_data.status_code) testing status code
+
+weather = weather_data.json()['weather'][0]['main']
+temp = round(weather_data.json()['main']['temp'])
+
+print(f"The weather in {user_input} is {weather}.")
+print(f"The temperature in {user_input} is {temp}°F.")
+
+
+
